f1dc84b316ca4ac16f494743c1a9b060b88c757d
GeonMo Yang <ands0927@naver.com>
2024-04-01 21:53:36 +0900

    Time: 981 ms (54.02%), Space: 0B (100.00%) - LeetHub

--- 1179-reformat-department-table/1179-reformat-department-table.sql
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -2,17 +2,17 @@
 
 SELECT 
     id,
-    Sum(CASE WHEN month = 'Jan' THEN revenue END) as Jan_Revenue,
-    Sum(CASE WHEN month = 'Feb' THEN revenue END) as Feb_Revenue,
-    Sum(CASE WHEN month = 'Mar' THEN revenue END) as Mar_Revenue,
-    Sum(CASE WHEN month = 'Apr' THEN revenue END) as Apr_Revenue,
-    Sum(CASE WHEN month = 'May' THEN revenue END) as May_Revenue,
-    Sum(CASE WHEN month = 'Jun' THEN revenue END) as Jun_Revenue,
-    Sum(CASE WHEN month = 'Jul' THEN revenue END) as Jul_Revenue,
-    Sum(CASE WHEN month = 'Aug' THEN revenue END) as Aug_Revenue,
-    Sum(CASE WHEN month = 'Sep' THEN revenue END) as Sep_Revenue,
-    Sum(CASE WHEN month = 'Oct' THEN revenue END) as Oct_Revenue,
-    Sum(CASE WHEN month = 'Nov' THEN revenue END) as Nov_Revenue,
-    Sum(CASE WHEN month = 'Dec' THEN revenue END) as Dec_Revenue   
+    Sum(CASE WHEN month = 'Jan' THEN revenue ELSE null END) as Jan_Revenue,
+    Sum(CASE WHEN month = 'Feb' THEN revenue ELSE null END) as Feb_Revenue,
+    Sum(CASE WHEN month = 'Mar' THEN revenue ELSE null END) as Mar_Revenue,
+    Sum(CASE WHEN month = 'Apr' THEN revenue ELSE null END) as Apr_Revenue,
+    Sum(CASE WHEN month = 'May' THEN revenue ELSE null END) as May_Revenue,
+    Sum(CASE WHEN month = 'Jun' THEN revenue ELSE null END) as Jun_Revenue,
+    Sum(CASE WHEN month = 'Jul' THEN revenue ELSE null END) as Jul_Revenue,
+    Sum(CASE WHEN month = 'Aug' THEN revenue ELSE null END) as Aug_Revenue,
+    Sum(CASE WHEN month = 'Sep' THEN revenue ELSE null END) as Sep_Revenue,
+    Sum(CASE WHEN month = 'Oct' THEN revenue ELSE null END) as Oct_Revenue,
+    Sum(CASE WHEN month = 'Nov' THEN revenue ELSE null END) as Nov_Revenue,
+    Sum(CASE WHEN month = 'Dec' THEN revenue ELSE null END) as Dec_Revenue   
 FROM Department
 GROUP BY id
