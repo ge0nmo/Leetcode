76c54cfa935f16d1604b3170bdfbd1e65d9e56ba
GeonMo Yang <ands0927@naver.com>
2024-05-19 16:06:15 +0900

    Time: 15 ms (46.14%), Space: 44.4 MB (89.53%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,33 +1,25 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
+        
+        int max = Math.max(solution(answerKey, k, 'T'), solution(answerKey, k, 'F'));
+        
+        return max;
+    }
+    
+    public int solution(String answerKey, int k, char ch)
+    {
         int lt = 0;
         int count = 0;
         int max = 0;
         
         for(int rt = 0; rt < answerKey.length(); rt++)
         {
-            if(answerKey.charAt(rt) == 'F')
+            if(answerKey.charAt(rt) == ch)
                 count++;
             
             while(count > k)
             {
-                if(answerKey.charAt(lt++) == 'F')
-                    count--;
-            }
-            
-            max = Math.max(max, rt - lt + 1);
-        }
-        
-        count = 0;;
-        lt = 0;
-        for(int rt = 0; rt < answerKey.length(); rt++)
-        {
-            if(answerKey.charAt(rt) == 'T')
-                count++;
-            
-            while(count > k)
-            {
-                if(answerKey.charAt(lt++) == 'T')
+                if(answerKey.charAt(lt++) == ch)
                     count--;
             }
             
