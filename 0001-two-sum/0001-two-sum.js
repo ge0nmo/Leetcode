d87993a6fec1e8ae70252c5fbe4019c6267b4bba
GeonMo Yang <ands0927@naver.com>
2024-05-26 13:18:42 +0900

    Time: 42 ms (99.43%), Space: 51.3 MB (28.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length - 1; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            const num = nums[i] + nums[j];
-            
-            if(num == target){
-                return [i, j]
-            }
+    const map = new Map();
+    
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(target - nums[i])){
+            return [map.get(target - nums[i]), i];
+        }else{
+            map.set(nums[i], i);
         }
     }
     
