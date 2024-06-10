e745904c9bc31ea78020db41ac1cde541b25ecbc
GeonMo Yang <ands0927@naver.com>
2024-06-10 16:17:50 +0900

    Time: 0 ms (100.00%), Space: 39.9 MB (82.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,21 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0)
-            return 0;
+        if(n <= 1)
+            return n;
         
-        if(n == 1 || n == 2)
-            return 1;
+		int a = 0, b = 1;
+		
+		while(n-- > 1)
+		{
+			int sum = a + b;
+			a = b;
+			b = sum;
+		}
         
-        int[] arr = new int[n + 1];
-        arr[1] = 1;
-        arr[2] = 1;    
-                
-        for(int i = 3; i <= n; i++)
-        {
-            arr[i] = arr[i - 1] + arr[i - 2];    
-        }
-        
-        return arr[n];
+        return b;
     }
     
 }
