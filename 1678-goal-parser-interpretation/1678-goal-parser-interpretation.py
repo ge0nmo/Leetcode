46159e4549dc9343c718b534925cd7b8f6b629a3
GeonMo Yang <ands0927@naver.com>
2024-06-20 17:49:02 +0900

    Time: 18 ms (18.40%), Space: 11.5 MB (83.33%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,5 +1,18 @@
 class Solution(object):
     def interpret(self, command):
+        # return command.replace('()', 'o').replace('(al)', 'al')
         
-        return command.replace('()', 'o').replace('(al)', 'al')
+        answer = ''
+        for i in range(len(command)):
+            if command[i] == 'G':
+                answer += 'G'
+            
+            elif command[i] == '(':
+                if command[i + 1] == ')':
+                    answer += 'o'
+                else:
+                    answer += 'al'
+            
+        
+        return answer
         
