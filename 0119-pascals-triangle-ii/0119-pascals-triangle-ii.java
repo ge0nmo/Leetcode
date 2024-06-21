3702d9b2251e15b9962565a0256b91b91954965f
GeonMo Yang <ands0927@naver.com>
2024-06-21 20:16:32 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (70.46%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,9 +1,10 @@
 class Solution {
     public List<Integer> getRow(int n) {
-        int[] arr = new int[n + 1];
         
+        int arr[] = new int[n + 1];
         arr[0] = 1;
-                
+        
+        
         for(int i = 1; i <= n; i++)
         {
             for(int j = i; j > 0; j--)
@@ -11,16 +12,24 @@ class Solution {
                 arr[j] += arr[j - 1];
             }
         }
-            
-        List<Integer> answer = new ArrayList();
-        for(int i = 0; i <= n; i++)
+        
+        List<Integer> list = new ArrayList();
+        for(int num : arr)
         {
-            if(arr[i] == 0)
+            if(num == 0)
                 break;
             
-            answer.add(arr[i]);            
+            list.add(num);
         }
         
-        return answer;
+        
+        return list;
+        
     }
 }
+
+
+// n번만큼 반복문을 돌린다
+// 이전 리스트를 가져온다
+// 양 옆은 1로 채운다
+// 가운데는 이전 리스트와 비교해서 구한다
