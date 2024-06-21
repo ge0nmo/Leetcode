5ecbcba7514485ab3d9fbe460e0baf5b41b6318b
GeonMo Yang <ands0927@naver.com>
2024-06-21 20:36:14 +0900

    Time: 0 ms (100.00%), Space: 40.1 MB (60.31%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,23 +1,21 @@
 class Solution {
     public int tribonacci(int n) {
         if(n == 0) return 0;
-        if(n == 1 || n == 2)
-            return 1;
-        
-        int[] arr = new int[n + 1];
-        arr[1] = 1;
-        arr[2] = 1;
-        
-        for(int i = 3; i <= n; i++)        
+        if(n == 1 || n == 2) return 1;
+                
+        int a = 0;
+        int b = 1;
+        int c = 1;        
+        int d = 0;
+        for(int i = 3; i <= n; i++)
         {
-            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
         }
         
-        return arr[n];
+        return d;
+        
     }
 }
-
-// n + 1 개의 배열을 생성
-// 아래 모든 수를 더하는 것이지만
-// 이전 배열이 모든 숫자를 더한 것의 합이기 때문에
-// 
