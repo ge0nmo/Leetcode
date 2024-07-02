81c4e6cc653050165c1b0c24350171f13a9d5f95
GeonMo Yang <ands0927@naver.com>
2024-07-03 00:41:00 +0900

    Time: 1 ms (99.43%), Space: 46 MB (50.23%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -15,8 +15,12 @@
  */
 class Solution {
     public List<TreeNode> allPossibleFBT(int n) {
+        return allPossibleFBT(n, new HashMap<>());
+    }
+    
+    public List<TreeNode> allPossibleFBT(int n, HashMap<Integer, List<TreeNode>> map) {
         List<TreeNode> list = new ArrayList();
-        
+                        
         if(n == 1)
         {
             list.add(new TreeNode(0));
@@ -27,11 +31,14 @@ class Solution {
             return list;
                
         
+        if(map.containsKey(n))
+            return map.get(n);
+        
         for(int i = 1; i < n; i += 2)
         {            
             int j = n - 1 - i;
-            List<TreeNode> left = allPossibleFBT(i);
-            List<TreeNode> right = allPossibleFBT(j);
+            List<TreeNode> left = allPossibleFBT(i, map);
+            List<TreeNode> right = allPossibleFBT(j, map);
             
             
             for(TreeNode node1 : left)
@@ -41,15 +48,13 @@ class Solution {
                     TreeNode node = new TreeNode(0);
                     node.left = node1;
                     node.right = node2;                                
-                    
+                                    
                     list.add(node);
                 }                
             }                        
         }
         
+        map.put(n, list);
         return list;
     }
-    
-    
-    
 }
