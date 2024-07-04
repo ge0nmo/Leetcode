7a61aa941c89501316affc22b1a7ffc3265d670b
GeonMo Yang <ands0927@naver.com>
2024-07-05 00:58:02 +0900

    Time: 42 ms (15.25%), Space: 12.3 MB (13.64%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,9 +1,26 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
-        step = [0, 0]
+        memo = [None] * len(cost)
         
-        for i in range(2, len(cost) + 1):
-            n = min(step[-1] + cost[i - 1], step[-2] + cost[i - 2])
-            step.append(n)
-            
-        return step[-1]
+        return min(self.solution(cost, 0, memo), self.solution(cost, 1, memo))
+        
+        
+    
+    
+    def solution(self, cost, index, memo):
+        if index >= len(cost):
+            return 0
+        
+        if memo[index] is not None:
+            return memo[index]
+        
+        step1 = self.solution(cost, index + 1, memo)
+        step2 = self.solution(cost, index + 2, memo)
+        
+        m = min(step1, step2) + cost[index]
+        memo[index] = m
+        
+        return m
+        
+        
+        
