69bce0b8eb72c5d96c5e9b80b7307011af20765c
GeonMo Yang <ands0927@naver.com>
2024-10-09 01:45:06 +0900

    Time: 2 ms (90.62%), Space: 62.8 MB (89.74%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,25 +15,40 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root == null){
+        if(root == null) {
             return 0;
+        } 
+        
+        int depth = 1;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while(!queue.isEmpty()){            
+            
+            int size = queue.size();
+            
+            for(int i = 0; i < size; i++){
+                TreeNode cur = queue.poll();
+                
+                if(cur.left == null && cur.right == null){
+                    return depth;
+                }
+                
+                if(cur.left != null){
+                    queue.add(cur.left);
+                }
+                
+                if(cur.right != null){
+                    queue.add(cur.right);
+                }
+                
+            }
+            
+            ++depth;                                    
         }
         
-        if(root.left == null && root.right == null){
-            return 1;
-        }
+        return depth;
         
-        
-        int min = Integer.MAX_VALUE;
-        
-        if(root.left != null){
-            min = Math.min(minDepth(root.left), min);    
-        }
-        
-        if(root.right != null){
-            min = Math.min(minDepth(root.right), min);    
-        }
-        
-        return min + 1;
     }
 }
