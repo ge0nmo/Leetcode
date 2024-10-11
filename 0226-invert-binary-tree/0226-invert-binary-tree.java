0df901adcb10d65e3921945c540713dcf09568c7
GeonMo Yang <ands0927@naver.com>
2024-10-11 13:50:14 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (36.32%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -19,13 +19,13 @@ class Solution {
             return null;
         }
         
-        if(root.left == null && root.right == null){
-            return root;
-        }
         
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
         
-        return new TreeNode(root.val, right, left);
+        root.left = right;
+        root.right = left;
+        
+        return root;        
     }
 }
