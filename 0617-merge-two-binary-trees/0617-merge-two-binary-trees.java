ff886cdf1f264e286ef3b9871ebf6aa1c9617671
GeonMo Yang <ands0927@naver.com>
2024-10-15 10:43:21 +0900

    Time: 0 ms (100.00%), Space: 44.7 MB (65.97%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -15,32 +15,19 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null){
-            return null;
+        if(root1 == null)
+            return root2;
+        
+        if(root2 == null){
+            return root1;
         }
         
-        int val = 0;
-        TreeNode left;
-        TreeNode right;
-        
-        if(root1 != null && root2 != null){
-            val = root1.val + root2.val;
-            left = mergeTrees(root1.left, root2.left);
-            right = mergeTrees(root1.right, root2.right);
-        } else if(root1 != null){
-            val = root1.val;
-            left = mergeTrees(root1.left, null);
-            right = mergeTrees(root1.right, null);
-        } else{
-            val = root2.val;
-            left = mergeTrees(null, root2.left);
-            right = mergeTrees(null, root2.right);
-        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
         
         
-        return new TreeNode(val, left, right);        
+        return root1;
     }
 }
 
-// 양쪽 모두가 null일때까지 아래로 내려간다
-// 한쪽이라도 존재하면 합침
