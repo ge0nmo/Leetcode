22dbe3ff6c13d5327414e7484dd157d0cb379ca6
GeonMo Yang <ands0927@naver.com>
2024-10-19 19:51:03 +0900

    Time: 2 ms (100.00%), Space: 51.7 MB (28.28%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function(root, x, y) {
+    let depth = 0;
+    const map = new Map();
+    
+    function dfs(node, x, y, parent, depth, map){
+        if(node === null){
+            return false;
+        }
+        
+        if(node.val === x || node.val === y){
+            map.set(node.val, [parent, depth]);
+        }
+        
+        if(map.get(x) !== undefined && map.get(y) !== undefined){
+            const xArray = map.get(x);
+            const yArray = map.get(y);
+            
+            
+            if(xArray[0] !== yArray[0] && xArray[1] === yArray[1]){
+                console.log(`x  = ${xArray}`);
+                console.log(`y  = ${yArray}`);
+                return true;
+            }
+        }
+        
+        return dfs(node.left, x, y, node.val, depth + 1, map) || dfs(node.right, x, y, node.val, depth + 1, map);
+    }
+    
+    return dfs(root, x, y, 0, 0, new Map());
+    
+};
