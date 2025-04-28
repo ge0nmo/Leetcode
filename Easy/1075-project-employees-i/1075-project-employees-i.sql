50c866323d89a50a527479b9b30a713c446322f1
GeonMo Yang <ands0927@naver.com>
2025-04-28 11:23:02 +0900

    Time: 509 ms (81.41%), Space: 0B (100%) - LeetHub

--- Easy/1075-project-employees-i/1075-project-employees-i.sql
+++ Easy/1075-project-employees-i/1075-project-employees-i.sql
@@ -2,13 +2,14 @@
 
 # group by project_id로 묶기
 # avg(experience_years)로 평균 구하기
+# 필요한 값만 가져오기
 
 SELECT
     p.project_id,
     ROUND(AVG(e.experience_years), 2) as average_years
 FROM
-    Employee e
+    Project p
 JOIN
-    Project p ON p.employee_id = e.employee_id
+    (SELECT employee_id, experience_years FROM Employee) e ON e.employee_id = p.employee_id
 GROUP BY
     p.project_id
