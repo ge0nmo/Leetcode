c22f64bbff5f2142917380bff89689025c7e03c0
GeonMo Yang <ands0927@naver.com>
2025-07-22 01:06:56 +0900

    Time: 1 ms (97.63%), Space: 59 MB (84.65%) - LeetHub

---
+++ Medium/0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    const answer = [];
+    backtrack(candidates, target, 0, answer, []);
+    return answer;
+};
+
+function backtrack(candidates, target, index, answer, cur){
+    if(target == 0){
+        answer.push([...cur]);
+        return;
+    }
+
+    if(target < 0) return;
+
+    for(; index < candidates.length; index++){
+        const candidate = candidates[index];
+        cur.push(candidate);
+        backtrack(candidates, target - candidate, index, answer, cur);
+        cur.pop();
+    }
+}
