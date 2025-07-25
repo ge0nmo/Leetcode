41228d793bb31a707f33e16a3155a00162896e01
GeonMo Yang <ands0927@naver.com>
2025-07-25 22:56:30 +0900

    Time: 27 ms (98.13%), Space: 41.5 MB (88.91%) - LeetHub

--- Medium/0041-word-search/0041-word-search.java
+++ Medium/0041-word-search/0041-word-search.java
@@ -1,59 +1,57 @@
 class Solution {
-    final int[] dx = {-1, 0, 1, 0};
-    final int[] dy = {0, 1, 0, -1};
+    static final int[] dx = {-1, 0, 1, 0};
+    static final int[] dy = {0, 1, 0, -1};
 
     public boolean exist(char[][] board, String word) {
-        if(!wordAvailable(board, word)) return false;
+        if(!isAvailable(board, word)) return false;
 
         boolean[][] visited = new boolean[board.length][board[0].length];
 
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-               if(board[i][j] == word.charAt(0)){
-                  if(backtrack(board, word, i, j, 0, visited)){
-                    return true;
-                  }
-               }
+        for(int x = 0; x < board.length; x++){
+            for(int y = 0; y < board[0].length; y++){
+                if(board[x][y] == word.charAt(0)){
+                    if(backtrack(board, word, x, y, 0, visited)) return true;
+                }
             }
         }
+
         return false;
     }
 
-    public boolean backtrack(char[][] board, String word, int row, int col, int idx, boolean[][] visited){        
-        if(idx == word.length()) return true;
-        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
-        if(visited[row][col]) return false;
-        if(board[row][col] != word.charAt(idx)) return false;
+    public boolean backtrack(char[][] board, String word, int x, int y, int index, boolean[][] visited)
+    {
+        if(word.length() == index) return true;
+        if(x < 0 || x >= board.length || y < 0 || y >= board[0].length) return false;
+        if(visited[x][y]) return false;
+        if(word.charAt(index) != board[x][y]) return false;
 
-        visited[row][col] = true;
+        visited[x][y] = true;
 
         for(int i = 0; i < 4; i++){
-            int nx = row + dx[i];
-            int ny = col + dy[i];
-            
-            if(backtrack(board, word, nx, ny, idx + 1, visited)) return true;
-
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(backtrack(board, word, nx, ny, index + 1, visited)){
+                return true;
+            }
         }
 
-        visited[row][col] = false;
+        visited[x][y] = false;
         return false;
     }
 
-    public boolean wordAvailable(char[][] board, String word)
+    public boolean isAvailable(char[][] board, String word)
     {
-        Map<Character, Integer> map = new HashMap<>();
+        List<Character> list = new ArrayList();
         for(char[] row : board){
             for(char ch : row){
-                map.put(ch, 1);
+                list.add(ch);
             }
         }
 
         for(char ch : word.toCharArray()){
-            if(!map.containsKey(ch)) return false;
+            if(!list.contains(ch)) return false;
         }
+
         return true;
     }
 }
-
-// 0, 0 부터 시작해서 하나씩 돌기
-// 0번째 인덱스에 있는 글자와 다르다면 리턴
